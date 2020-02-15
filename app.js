0c057c4a9414b19c4394b35499b87118fb74d0ab
ankita <ankitapakki921@gmail.com>
2020-02-15 12:04:07 +0530

    node-api(app.js) MongoDB Connection

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const app = express();
-const postRoute= require('./routes/post')
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const dotenv= require('dotenv');
 
 
+dotenv.config();
+
+
+mongoose
+    .connect(process.env.MONGO_URI,{useNewUrlParser:true})
+    .then(() => console.log("DB Connected"));
+
+mongoose
+    .connection.on("error",err => {
+
+        console.log(`DB Connection error:${err.message}`);
+
+    });
+
+    //bring in routes
+    const postRoute= require('./routes/post');
+
+    //middleware
+
+app.use(morgan("dev"));
 app.use("/",postRoute);
 
 
-app.listen(8080);
+const port=8080;
+app.listen(port,()=>{
+
+console.log(`a node.js api is on 8080`);
+
+});
+
